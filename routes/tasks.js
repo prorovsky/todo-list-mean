318e901b85fda8647173e45c2980e228e23c9287
Pavel Prorovsky <prorovsky@gmail.com>
2017-01-21 20:00:39 +0300

    add database

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,16 @@
 let express = require('express'),
+    mongojs = require('mongojs'),
+    config = require('./config'),
+    db = mongojs(config.DBURL, ['todos']),
     router = express.Router();
 
 router.get('/tasks', (req, res) => {
-    res.send('tasks');
+    db.tasks.find((err, todos) =>{
+        if(err){
+
+        }
+        res.json(todos);
+    });
 });
 
 module.exports = router;
